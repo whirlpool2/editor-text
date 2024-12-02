964572372863b17152e35e72e4f1d0b9922985b1
whirlpool2 <stefan.mihalache@student.uaic.ro>
2024-12-02 14:38:13 +0200

    Reformatare și clarificare la câteva comentarii.

--- EditorText/SFML/EditorText.cpp
+++ EditorText/SFML/EditorText.cpp
@@ -1,17 +1,11 @@
 ﻿#include <iostream>
 #include <conio.h>
 #include <SFML/Graphics.hpp>
-#include<SFML/Window.hpp>
+#include <SFML/Window.hpp>
 
 using namespace std;
 using namespace sf;
-//etrtvervwervwervwervwerv
-//test
 
-//test !!!!!!!!!
-//sigma on th wall
-
-//test mihai
 struct RGBColor
 {
     int r;
@@ -47,7 +41,6 @@ Font loadFont(string path)
 
     if (!(font.loadFromFile(path)))
     {
-        //throw("Font " + path + " could not be found.");
         cerr << "Font " << path << " could not be found." << '\n';
     }
 
@@ -64,16 +57,20 @@ void setFont(Text& text, Font& font, int size, RGBColor color, string path)
     text.setPosition(10, 10);
 }
 
-//fac o functie pentru a evidentia cuvantul curent pe care suntems
+// Funcție care evidențiază cuvântul curent.
 void CurrentWord(string& currentText, int cursorPos, Text& text, Text& currWord) {
 
     int start = cursorPos;
     while (start > 0 && currentText != ' ')
+    {
         start--;
+    }
 
     int end = cursorPos;
     while (end < currentText.length() && currentText[end] != ' ')
+    {
         end++;
+    }
 
     string before = currentText.substr(0, start);
     string current = currentText.substr(start, end - start);
@@ -85,7 +82,7 @@ void CurrentWord(string& currentText, int cursorPos, Text& text, Text& currWord)
     currWord.setPosition(startPos);
 }
 
-// Ia input de la tastatură.
+// Funcție care primește informații de la tastatură și modifică documentul corespunzător.
 void handleKeyboardInput(RenderWindow& Window)
 {
     string currentText = "";
@@ -94,34 +91,48 @@ void handleKeyboardInput(RenderWindow& Window)
     Text text, CuvCurent;
     setFont(text, font, 24, { 255, 128, 64 }, "Fonts/CascadiaMono.ttf");
     setFont(CuvCurent, font, 24, { 255, 255 , 255 }, "Fonts/CascadiaMono.ttf");
-    while (Window.isOpen()) {
+    while (Window.isOpen())
+    {
         Event event;
-        while (Window.pollEvent(event)) {
+        while (Window.pollEvent(event)) // Verificăm event-ul curent.
+        {
             if (event.type == Event::Closed)
+            {
                 Window.close();
+            }
 
-            // Handle keyboard input for characters
+            // Dacă event-ul curent presupune caractere date de la tastatură...
             if (event.type == Event::TextEntered) {
-                if (event.text.unicode < 128) {
+                if (event.text.unicode < 128)
+                {
                     char key = static_cast<char>(event.text.unicode);
-                    if (key == 8 && !currentText.empty() && cursorPos > 0) {
-                        // daca sterg caracterul scad si pozitia c
+
+                    // Dacă se apasă backspace și se poate șterge...
+                    if (key == 8 && !currentText.empty() && cursorPos > 0)
+                    {
                         currentText.erase(cursorPos - 1, 1);
-                        cursorPos--;
+                        cursorPos--; // Devansăm cu o poziție după ștergere.
                     }
-                    else if (key >= 32 && key <= 126) {
-                        // adaug la pozitia cursorului caracterul de la tasta
+                    else if (key >= 32 && key <= 126)
+                    {
+                        // key in [32, 126] implică caracterele alfanumerice, punctuații și paranteze, etc.
+                        // După inserarea caracterului, se incrementează poziția cursorului.
                         currentText.insert(cursorPos, 1, key);
                         cursorPos++;
                     }
                 }
-                text.setString(currentText); // Update the displayed text
+                text.setString(currentText); // După ce se efectuează operațiile, actualizăm textul vizibil.
             }
-            if (event.type == Event::KeyPressed) {
+            if (event.type == Event::KeyPressed) // Acest caz tratează tastele ce nu produc caractere.
+            {
                 if (event.key.code == Keyboard::Left && cursorPos > 0)
+                {
                     cursorPos--;
+                }
                 else if (event.key.code == Keyboard::Right && cursorPos < currentText.size())
+                {
                     cursorPos++;
+                }
             }
         }
 
@@ -137,7 +148,7 @@ void handleKeyboardInput(RenderWindow& Window)
 int main()
 {
     RenderWindow EditorText(VideoMode(800, 800), "Editor Text", sf::Style::Default);
-    EditorText.setFramerateLimit(144);
+    EditorText.setFramerateLimit(60);
 
     handleKeyboardInput(EditorText);
 
